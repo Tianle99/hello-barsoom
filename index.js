05bdc8e505b9e2b7b160e7b6544208f02269ea81
Tianle Liu <128502545+Tianle99@users.noreply.github.com>
2024-07-17 20:50:23 +0000

    initial version

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
+try {
+    const name = core.getInput('name');
+    const output_val = `Hello ${name}!`;
+    core.setOutPut("greeting", output_val)
 
-const name = core.getInput('name');
-const output_val = `Hello ${name}!`;
-core.setOutPut("greeting", output_val)
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+} catch (error) {
+    core.setFailed(error.message);
+}
